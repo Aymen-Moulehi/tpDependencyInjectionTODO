8b5ed2b1268556fe0f68057b2a4b1fd022f6c8f6
aymen <aymen.moulehi@outlook.com>
2023-02-15 11:18:24 +0100

    Initial commit

---
+++ src/main/java/com/esprit/Dependency/tpDependencyInjection/dao/IToDoDao.java
@@ -0,0 +1,9 @@
+package com.esprit.Dependency.tpDependencyInjection.dao;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+public interface IToDoDao {
+	public List<String> getCoursesList();
+}
